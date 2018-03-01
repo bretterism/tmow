7ffa7d9bf3ae5d2517f281e3d8922afa743cd263
Brett Hayes <brett.james.hayes@gmail.com>
2018-03-01 11:19:50 -0800

    fixing things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { PageHeader, Row, Col } from 'react-bootstrap';
 
 import api from './api'
-import Tmow from './tmow'
-
-const defaultImgUrl = 'img/default-avatar.png';
+import TmowConatiner from './tmow'
 
 class App extends Component {
   constructor(props) {
@@ -20,14 +17,9 @@ class App extends Component {
   }
 
   render() {
-    const teamMembers = this.state.teams.map((team) => {
-      return <Col md={4}><Tmow teamName={team.teamName} fullName={team.fullName} imgUrl={ team.imgUrl || defaultImgUrl } teamColor={team.teamColor}/></Col>;
-    });
-
     return (
-      <div>
-        <PageHeader style={{textAlign: 'center'}}>Team Members of the Week!</PageHeader>
-        <Row>{ teamMembers }</Row>
+      <div style={{padding: '25px'}}>
+        <TmowConatiner />
       </div>
     );
   }
